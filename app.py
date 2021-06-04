1f0b1d62d0cebe05b38cfeaffa948c4ebb70665d
zhengz55 <43350334+zhengz55@users.noreply.github.com>
2021-06-04 15:48:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,32 @@ def add_header(r):
     return r
 
 
+# Example trigger
+@app.route("/additem", methods=['POST'])
+def AddItem():
+    """
+    Expects request parameters in the form of ?itemName={string}
+    """
+    if request.method == 'POST':
+        food_item = request.args.get('itemName')
+        
+        # Retrieve food item here. IDK how to work with excel/csv in python nor do I currently have our storage format
+        food_obj = 1 
+        # Expecting an object at the end here
+        return jsonify(food_obj)
+    
+@app.route("/getall", methods=['get'])
+def AddItem():
+    if request.method == 'GET':
+        # TODO
+    return None
+
+@app.route("/findfoodcombination", methods=['POST'])
+def FindRandomFoodCombination():
+    if request.method == 'POST':
+        # TODO
+    return None
+
 
 if __name__ == '__main__':
     app.run(debug=True)
